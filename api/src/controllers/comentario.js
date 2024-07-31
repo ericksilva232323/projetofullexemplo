8134399c52c99dddc6aa9da5b56528da08add3f2
ericksilva232323 <erick14silva14@gmail.com>
2024-07-31 11:15:28 -0300

    ok

--- api/src/controllers/comentario.js
+++ api/src/controllers/comentario.js
@@ -1,4 +1,24 @@
 const { PrismaClient } = require('@prisma/client');
+const { read, create, update, del } = require('./comentario');
 const prisma = new PrismaClient();
 
-module.exports = {};
+    const read = async (req, res) => {
+        if (req.params.id !== undefined){
+            const comentario = await prisma.comentario.findUnique({
+                where: {
+                    id: req.params.matricula
+                }
+            });
+            return res.json(comentario);
+        } else {
+            const comentario = await prisma.colaborador.findMany();
+            return res.json(comentario)
+        }
+    };
+
+module.exports = {
+    read,
+    create,
+    update,
+    del
+};
--- api/src/routes.js
+++ api/src/routes.js
@@ -5,9 +5,19 @@ const router = express.Router();
 const Middleware = require('./middleware/middleware');
 const Colaborador = require('./controllers/colaborador');
 const Os = require('./controllers/os');
-const Comentario = require('./controllers/comentario');
+const Comentario = require('./controllers/co mentario');
 
 router.post('/login', Colaborador.login);
+router.post('/colaborador', Colaborador.create);
+router.get('/colaborador', Middleware.validaAcesso, Colaborador.read);
+router.get('/colaborador/:matricula', Middleware.validaAcesso, Colaborador.read);
+router.put('/colaborador', Middleware.validaAcesso, Colaborador.update);
+router.delete('/colaborador/:matricula', Middleware.validaAcesso, Colaborador.del);
+
+router.post('/comentario', Comentario.create);
+router.get('/comentario', Comentario.read);
+router.put('/comentario', Comentario.update);
+router.delete('/comentario', Comentario.del);
 
 router.get('/', (req, res) => { return res.json("API OSs respondendo") });
 
